b94a2dc6190dd14f470164abf1f227ac09462b7c
MD ABDULLAH <58176575+abdullah-cse@users.noreply.github.com>
2022-09-20 20:42:03 +0600

    Define Collection

---
+++ lib/collections/routine.dart
@@ -0,0 +1,18 @@
+import 'package:isar/isar.dart';
+import 'package:isar_example/collections/category.dart';
+
+@Collection()
+class Routine {
+  Id id = Isar.autoIncrement;
+
+  late String title;
+
+  @Index()
+  late DateTime startTime;
+
+  @Index(caseSensitive: false)
+  late String day;
+
+  @Index(composite: [CompositeIndex('title')])
+  final category = IsarLink<Category>();
+}
